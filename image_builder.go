ea426c19db287cb48c41ba62a37100fafb1bb45a
Austin <acmacalister@gmail.com>
2015-04-13 14:51:04 -0700

    some clean up worker

---
+++ image_builder.go
@@ -0,0 +1,177 @@
+package bay
+
+import (
+	"archive/tar"
+	"bytes"
+	"io"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/fsouza/go-dockerclient"
+	"github.com/libgit2/git2go"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+
+//const (
+//  GolangEnv = iota // our environment types.
+//  rubyEnv
+//  railsEnv
+//  pythonEnv
+//  djangoEnv
+//)
+
+// tarBuilder is used for building a tar file required by
+// the Docker API to build an image off a Dockerfile.
+type tarBuilder struct {
+	tr      *tar.Writer
+	rootDir string
+}
+
+// pathWalker is the helper function used for filepath Walk called in BuildWithGitRepo.
+func (tb *tarBuilder) pathWalker(path string, info os.FileInfo, err error) error {
+	if info.IsDir() || strings.HasPrefix(".git", path) {
+		return nil // skip this one.
+	}
+
+	name, err := filepath.Rel(tb.rootDir, path)
+	if err != nil {
+		return err
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	now := time.Now()
+	header := &tar.Header{Name: name, Size: info.Size(), Mode: int64(info.Mode()),
+		ModTime: info.ModTime(), AccessTime: now, ChangeTime: now,
+	}
+	if err := tb.tr.WriteHeader(header); err != nil {
+		return err
+	}
+
+	if _, err := io.Copy(tb.tr, file); err != nil {
+		return err
+	}
+	return nil
+}
+
+// BuildWithGitRepo takes a git url to build a docker image.
+// Returns the container ID of the newly built docker image.
+func BuildWithGitRepo(gitUrl string) (string, error) {
+	client, err := docker.NewClient("unix:///var/run/docker.sock") // probably need to be configurable.
+	if err != nil {
+		return "", err
+	}
+	directoryName := randomStringName()
+	os.Mkdir(directoryName, os.ModePerm)
+
+	if _, err := git.Clone(gitUrl, directoryName, &git.CloneOptions{}); err != nil {
+		os.RemoveAll(directoryName)
+		return "", err
+	}
+
+	inputbuf, outputbuf := bytes.NewBuffer(nil), bytes.NewBuffer(nil)
+
+	tb := tarBuilder{tr: tar.NewWriter(inputbuf), rootDir: directoryName}
+	if err := filepath.Walk(directoryName, tb.pathWalker); err != nil {
+		os.RemoveAll(directoryName)
+		return "", err
+	}
+
+	tb.tr.Flush()
+	tb.tr.Close()
+	opts := docker.BuildImageOptions{
+		Name:         "name",
+		InputStream:  inputbuf,
+		OutputStream: outputbuf,
+	}
+	if err := client.BuildImage(opts); err != nil {
+		os.RemoveAll(directoryName)
+		return "", err
+	}
+
+	os.RemoveAll(directoryName)
+	return "", nil
+}
+
+// randomStringName generates a random 32 character string.
+func randomStringName() string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	b := make([]rune, 32)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+// randomPortNumber picks a port between 30,000 to 31,000. Going forward we will need a way of picking ports deterministically.
+//func randomPortNumber() map[string]struct{} {
+//  rand.Seed(time.Now().Unix()) // not sure we need to see again, but w/e.
+//  min := 31000
+//  max := 30000
+//  var s struct{}
+//  ports := make(map[string]struct{})
+//  randomPort := fmt.Sprintf("%d", rand.Intn(max-min)+min)
+//  ports[randomPort] = s // not sure what the struct is for.
+//  return ports
+//}
+
+// getDockerConfig returns a docker config depending on what environment/language chosen.
+//func getDockerConfig(environment int) (*dockerclient.ContainerConfig, error) {
+//  buildImage := ""
+//  switch environment {
+//  case GolangEnv:
+//    buildImage = "golang:onbuild"
+//  case rubyEnv:
+//    buildImage = "rails:onbuild"
+//  case railsEnv:
+//    buildImage = "ruby:onbuild"
+//  case pythonEnv:
+//    buildImage = "python:onbuild"
+//  case djangoEnv:
+//    buildImage = "django:onbuild"
+//  default:
+//    return nil, errors.New("not a valid environment.")
+//  }
+
+//  return &dockerclient.ContainerConfig{Image: buildImage, Cmd: []string{"bash"}, ExposedPorts: randomPortNumber()}, nil
+//}
+
+// BuildWithFiles takes files and an environment to build a docker image.
+// Returns the container ID of the newly built docker image.
+//func BuildWithFiles(environment int, files [][]byte, fileNames []string) (string, error) {
+//  client, err := dockerclient.NewDockerClient("unix:///var/run/docker.sock", nil) // probably need to be configurable.
+//  if err != nil {
+//    return "", err
+//  }
+//  directoryName := randomStringName()
+//  os.Mkdir(directoryName, os.ModePerm)
+//  if len(fileNames) != len(files) {
+//    return "", errors.New("mismatch file bytes and name.")
+//  }
+//  for i, file := range files {
+//    f, err := os.Create(directoryName + "/" + fileNames[i])
+//    if err != nil {
+//      return "", err
+//    }
+//    if _, err = f.Write(file); err != nil { // write the file (bytes we got from the upload).
+//      return "", err
+//    }
+//  }
+//
+//  dockerConfig, err := getDockerConfig(environment)
+//  if err != nil {
+//    return "", err
+//  }
+//  containerId, err := client.CreateContainer(dockerConfig, "something cool") // need a better name.
+//  if err != nil {
+//    return "", err
+//  }
+//
+//  return containerId, nil
+//}
--- server.go
+++ server.go
@@ -1,50 +1,52 @@
-package main
+package bay
 
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/acmacalister/helm"
-	"github.com/libgit2/git2go"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+
+	"github.com/acmacalister/helm"
 )
 
-func main() {
-	r := helm.New(fallThrough)
-	r.Handle("POST", "/web_hook", root)
-
-	http.ListenAndServe(":8080", r)
+// server contains all the info for running the API endpoints
+// and web dashboard.
+type server struct {
+	workers []string //addresses to each of the workers.
 }
 
+// Start takes a slice of worker's addresses (e.g http://worker1:8080)
+// and a listener for which port to run this server now.
+func Start(workers []string, listener string) {
+	s := new(server)
+	s.workers = append(s.workers, workers...)
+	r := helm.New(fallThrough)
+	r.POST("/web_hook", s.webHookHandler)
+	r.Run(listener)
+}
+
+// fallThrough is the caught all route for endpoints called without a handler.
 func fallThrough(w http.ResponseWriter, r *http.Request, params url.Values) {
 	fmt.Fprintf(w, "404 - Not found")
 }
 
-func root(w http.ResponseWriter, r *http.Request, params url.Values) {
-
+// webHookHandler takes the response from Github webhook callbacks.
+func (s *server) webHookHandler(w http.ResponseWriter, r *http.Request, params url.Values) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
-	//fmt.Println(string(body))
+
 	var wh webhook
 	err = json.Unmarshal(body, &wh)
 	if err != nil {
 		fmt.Println(err)
 	}
-	//fmt.Println(wh)
+
 	fmt.Fprintf(w, "ok")
 	fmt.Println("Post trigger")
-	cloneProject(wh.Repository.URL, wh.Repository.Name)
-}
-
-func cloneProject(url, name string) {
-	fmt.Printf("Cloning down: %s\nUrl: %s\n", name, url)
-	_, err := git.Clone(url, name, &git.CloneOptions{})
-	if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("Clone successfull")
-	}
+	// we will need some logic to pick which worker will get the job.
+	// for now it is just the first worker.
+	http.Post(s.workers[0], bodyType, body)
 }
